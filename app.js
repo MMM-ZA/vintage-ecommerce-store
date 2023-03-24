fb9c074c93c876a18fec043ff918b6d8869b5a57
makoma mabilo <makoma44@gmail.com>
2023-03-24 16:21:15 +0800

    added styling to coupon/cart page

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const openBar = document.getElementById('bar');
 const closeBar = document.getElementById('close');
 const navbarBar = document.getElementById('navbar');
 
+
 if (openBar) {
   openBar.addEventListener('click',() => {
     navbarBar.classList.add('active');
@@ -12,3 +13,9 @@ if (closeBar) {
     navbarBar.classList.remove('active');
   })
 }
+
+// Add a click event listener to the icon
+const iconNavigation = documet.getElementById('shopping-icon');
+iconNavigation.addEventListener('click',()=> {
+    window.location.href = 'sproduct.html';
+});
